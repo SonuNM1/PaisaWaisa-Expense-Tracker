9361b895d28df3c20687538d7324b720d7f8304c
Sonu N Mahto <139614905+SonuNM1@users.noreply.github.com>
2025-03-24 23:02:26 +0530

    24 march

--- frontend/src/pages/Dashboard/ExpenseTransactions.jsx
+++ frontend/src/pages/Dashboard/ExpenseTransactions.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { LuArrowRight } from 'react-icons/lu'
+import moment from 'moment'
+import TransactionInfoCard from '../Cards/TransactionInfoCard'
 
 const ExpenseTransactions = ({transactions, onSeeMore}) => {
   return (
@@ -10,6 +12,23 @@ const ExpenseTransactions = ({transactions, onSeeMore}) => {
                 See All <LuArrowRight className='text-base'/>
             </button>
         </div>
+
+        <div className='mt-6'>
+          {
+            transactions?.slice(0, 4)?.map((expense) => (
+              <TransactionInfoCard
+                key={expense._id}
+                title={expense.category}
+                icon={expense.icon}
+                date={moment(expense.date).format('Do MMM YYYY')}
+                amount={expense.amount}
+                type='expense'
+                hideDeleteBtn
+              />
+            ))
+          }
+        </div>
+
     </div>
   )
 }
--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -11,6 +11,9 @@ import { addThousandsSeparator } from '../../utils/helper'
 import RecentTransactions from '../../components/Dashboard/RecentTransactions'
 import FinanceOverview from '../../components/Dashboard/FinanceOverview'
 import ExpenseTransactions from './ExpenseTransactions'
+import last30DaysExpenses from '../../components/Dashboard/last30DaysExpenses'
+import RecentIncomeWithCharts from '../../components/Dashboard/RecentIncomeWithCharts'
+import RecentIncome from '../../components/Dashboard/RecentIncome'
 
 const Home = () => {
 
@@ -50,7 +53,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu='Dashboard'>
       <div className='my-5 mx-auto'> 
-        {/* <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
           <InfoCard
             icon={<IoMdCard/>}
             label='Total Balance'
@@ -69,10 +72,11 @@ const Home = () => {
             value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
             color='bg-red-500'
           />
-        </div> */}
+        </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
-          {/* <RecentTransactions
+
+          <RecentTransactions
             transactions={dashboardData?.RecentTransactions}
             onSeeMore={() => navigate('/expense')}
           />
@@ -81,11 +85,25 @@ const Home = () => {
             totalBalance={dashboardData?.totalBalance || 0}
             totalIncome={dashboardData?.totalIncome || 0}
             totalExpense={dashboardData?.totalExpenses || 0}
-          /> */}
+          />
 
           <ExpenseTransactions
             transactions={dashboardData?.last30DaysExpenses?.transactions || []}
             onClick={() => navigate('/expense')}
+          />
+
+          <last30DaysExpenses
+            data={dashboardData?.last30DaysExpenses?.transactions || []}
+          />
+
+          <RecentIncomeWithCharts
+            data={dashboardData?.last60DaysIncome?.transactions?.slice(0, 4) || []}
+            totalIncome={dashboardData?.totalIncome || 0}
+          />
+
+          <RecentIncome
+            transactions={dashboardData?.last60DaysIncome?.transactions || []}
+            onSeeMore={() => navigate('/income')}
           />
 
         </div>
